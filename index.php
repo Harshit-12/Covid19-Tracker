3fad5ecf6ac5250c16901c224e242064b56c7621
Harshit-12 <64789508+Harshit-12@users.noreply.github.com>
2021-05-16 18:14:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <style type="text/css">
-    
-    input[type=submit] {
-    width: 50%;
-    background-color: #4CAF50;
-    color: white;
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Covid19 Report</title>
+	<style type="text/css">
 
-    padding: 14px 20px;
-    margin: 8px 0;
-    border-radius: 40px;
-    cursor: pointer;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  }
-  input[type=submit]:hover {
-    background-color: #45a049;
-    
+.state {
+  background-image: url('cool-background3.png');
+  width: 70%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+ border-radius: 20px;
+ background-size: auto;
+}
+.name {
+  background-image: url('cool2.png');
+  width: 60%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  text-align: center;
+ border-radius: 20px;
+ background-size: auto;
+}
+.name2 {
+  background-color: #ffff00;
+  width: 100%;
+  text-align: center;
+ border-top-left-radius: 20px;
+ border-top-right-radius: 20px;
+ background-size: auto;
+}
+.name3 {
+ background-color: #0066ff;
+  width: 100%;
+  text-align: center;
+ border-top-left-radius: 20px;
+ border-top-right-radius: 20px;
+ background-size: auto;
+}
+.name5 {
+  background-image: url('cool-background.png');
+  width: 100%;
+  text-align: center;
+   border-top-left-radius: 20px;
+ border-top-right-radius: 20px;
+ background-size: auto;
+}
+.name1 {
+  background-color: #2eb8b8;
+  width: 100%;
+  text-align: center;
+ border-top-left-radius: 20px;
+ border-top-right-radius: 20px;
+ background-size: auto;
+}
+.name4 {
+  background-color:#ff9900 ;
+  width: 100%;
+  text-align: center;
+ border-top-left-radius: 20px;
+ border-top-right-radius: 20px;
+ background-size: auto;
+}
 
-  }
-div {
+.header {
+  background-image: url('cool-background.png');
+  width: 40%;
+  font-family: 'Niconne';
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+ border-radius: 20px;
+ background-size: auto;
+}
+.middle {
   margin-top: 60px;
 opacity: 1.0;
 
@@ -31,108 +81,103 @@ opacity: 1.0;
  background-size: auto;
  padding: 20px;
 }
-.header {
-  background-image: url('cool-background.png');
-  width: 40%;
-  font-family: 'Niconne';
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
- border-radius: 20px;
- background-size: auto;
+th{
+	padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+	}
+td,th
+{
+	border: 1px solid #ddd;
+  padding: 8px;
 }
-.fontstyle{
-  width: 80%;
-  font-size: 15rem;
-      text-align: center;
-      height:10%;
-      line-height: 9vh;
-      color: #fcedd8;
-      background: #d52e3f;
-      font-family: 'Niconne';
-      font-weight: 70px;
-      text-shadow: 5px 5px 0px #eb452b, 
-                  10px 10px 0px #efa032, 
-                  15px 15px 0px #46b59b, 
-                  20px 20px 0px #017e7f, 
-                  25px 25px 0px #052939, 
-                  30px 30px 0px #c11a2b, 
-                  35px 35px 0px #c11a2b, 
-                  40px 40px 0px #c11a2b, 
-                  45px 45px 0px #c11a2b;
-}
-body {
-  background-image: url('cool.png');
-  background-size: auto;
-}
-@media (max-width: 480px) {
-    body {
-        background-image: url('cool.png');
-    }
-}
+tr:nth-child(even){background-color: #f2f2f2;}
 
-@media (min-width: 481px) and (max-width: 1024px) {
-    body {
-        background-image: url('cool.png');
-    }
-}
-</style>
+tr:hover {background-color: #ddd;}
+	</style>
 </head>
 <body>
-<br>
-<br>
 <center>
-  <div class="header"><h1>Covid19 Tracker</h1></div>
+<div class="header"><h1>Covid19 Tracker</h1></div>
+<div class="middle"><h2>Latest Updates of Covid19 about States and Union Territories of India</h2>
+</div>
+<div style="overflow-x:auto;">	
+	<table border="1px " >
+<?php
 
-<div>
 
-<h2>Get latest Updates of Covid19 about States and Union Territories of India</h2>
+$data=file_get_contents('https://api.covid19india.org/data.json');
+$coronalive =json_decode($data,true);
+//echo $coronalive['statewise'][1]['state'];
 
-<form action="Covid live.php" method="post">
-  <label for="cars"><strong> <h3>Choose  Location </h3> </strong></label>
-  
-  <select name="place">
-    <option value="Maharashtra"><b>Maharashtra</b></option>
-    <option value="Kerala">Kerala</option>
-    <option value="Karnataka">Karnataka</option>
-    <option value="Andhra Pradesh">Andhra Pradesh</option>
-    <option value="Tamil Nadu">Tamil Nadu</option>
-    <option value="Delhi">Delhi</option>
-    <option value="Uttar Pradesh">Uttar Pradesh</option>
-    <option value="West Bengal">West Bengal</option>
-    <option value="Odisha">Odisha</option>
-    <option value="Rajasthan">Rajasthan</option>
-    <option value="Chhattisgarh">Chhattisgarh</option>
-    <option value="Telangana">Telangana</option>
-    <option value="Haryana">Haryana</option>
-    <option value="Gujarat">Gujarat</option>
-    <option value="Bihar">Bihar</option>
-    <option value="Madhya Pradesh">Madhya Pradesh</option>
-    <option value="Assam">Assam</option>
-    <option value="Punjab">Punjab</option>
-    <option value="Jammu and Kashmir">Jammu and Kashmir</option>
-    <option value="Jharkhand">Jharkhand</option>
-    <option value="Uttarakhand">Uttarakhand</option>
-    <option value="Himachal Pradesh">Himachal Pradesh</option>
-    <option value="Goa">Goa</option>
-    <option value="Puducherry">Puducherry</option>
-    <option value="Tripura">Tripura</option>
-    <option value="Manipur">Manipur</option>
-    <option value="Chandigarh">Chandigarh</option>
-    <option value="Arunachal Pradesh">Arunachal Pradesh</option>
-    <option value="Meghalaya">Meghalaya</option>
-    <option value="Nagaland">Nagaland</option>
-    <option value="Ladakh">Ladakh</option>
-    <option value="Sikkim">Sikkim</option>
-    <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
-    <option value="Mizoram">Mizoram</option>
-    <option value="Dadra and Nagar Haveli and Daman and Diu">Dadra and Nagar Haveli and Daman and Diu</option>
-    <option value="Lakshadweep">Lakshadweep</option>
-        
-  
-  </select>
-  
-  <br> <br>
-  <input type="submit" value="Check">
-</form>
+$satecount = count($coronalive['statewise']);
+
+	
+?>
+
+	<tr>
+		<th>
+				State
+		</th>
+
+		<th>
+				Last Updated Date Time
+		</th>
+		
+		<th>
+
+				Confirmed Cases
+		</th>
+
+		<th>
+
+				Active Cases
+		</th>
+		<th>
+
+				Recovered Cases
+		</th>
+		<th>
+
+				Death Cases
+		</th>
+
+
+	</tr>
+
+
+	<?php   
+	$i=1;
+	while($i<38)
+	{ ?>
+	<tr>
+	
+	<td> <?php echo $coronalive['statewise'][$i]['state'] ?></td>
+	
+	
+	<td><?php echo $coronalive['statewise'][$i]['lastupdatedtime']  ?></td>>
+	
+	
+	
+	<td><?php echo $coronalive['statewise'][$i]['confirmed'] ?></td>
+	
+	
+	<td><?php echo $coronalive['statewise'][$i]['active'] ?></td>
+	
+	<td><?php echo  $coronalive['statewise'][$i]['recovered'] ?></td>
+
+	<td><?php echo $coronalive['statewise'][$i]['deaths'] ?></td>
+
+	</tr>
+	
+	<?php  $i++;
+}
+	?>
+	
+
+</table>
 </div>
 </center>
 </body>
